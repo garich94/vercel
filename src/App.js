484dbe3d4569b5fce5831ab7d0ca5b5a8436b055
garich94 <114189622+garich94@users.noreply.github.com>
2023-03-02 22:26:39 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.scss";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Все будет хорошо</p>
-        <p>Саша всё получается</p>
-      </header>
+      <div>
+        <h2>Счетчик: {count}</h2>
+        <h1>{count}</h1>
+        <button
+          disabled={count === 00}
+          onClick={() => setCount(count - 1)}
+          className="minus"
+        >
+          - Минус
+        </button>
+        <button onClick={() => setCount(count + 1)} className="plus">
+          Плюс +
+        </button>
+        <button onClick={() => setCount(0)} className="sbros">
+          Сброс
+        </button>
+      </div>
     </div>
   );
 }
